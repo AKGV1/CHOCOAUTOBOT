96d74bf34452cc61639cb4264352da5a9159dc0c
AKGV1 <97513152+AKGV1@users.noreply.github.com>
2022-03-27 08:43:47 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -54,6 +54,14 @@ async def start(client: Client, message: Message):
             ]
         ),
     )
+    
+    @Client.on_message(commandpro(["akg love you"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    await message.reply_message(
+            text="I LOVE TOO BABY",
+        
+    )
+    
    
 
 
